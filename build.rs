e81a11396807b838bd3b33c9fa6156d9e1295a65
Joe Kaushal <joe.kaushal@gmail.com>
2020-07-21 13:04:46 +0100

    Add dll to output dir

--- build.rs
+++ build.rs
@@ -1,10 +1,19 @@
 use std::env;
+use std::fs;
 use std::path::Path;
 
 fn main() {
     let root_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let lib_dir = Path::new(&root_dir).join("interception-1.0.1");
+    let out_dir = env::var("OUT_DIR").unwrap();
 
-    println!("cargo:rustc-link-search={}", lib_dir.join("x64").to_str().unwrap());
-    println!("cargo:rustc-link-lib=interception");
+    let lib = Path::new(&root_dir)
+        .join("interception-1.0.1")
+        .join("x64")
+        .join("interception.dll");
+
+    let lib_out = Path::new(&out_dir).join("interception.dll");
+    fs::copy(&lib, &lib_out).unwrap();
+
+    println!("cargo:rustc-link-search={}", out_dir);
+    println!("cargo:rustc-link-lib=dylib=interception");
 }
