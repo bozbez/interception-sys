e57fb17eb698d0a578118f00547a710e7e057f08
Joe Kaushal <joe.kaushal@gmail.com>
2020-07-21 13:16:24 +0100

    Copy static lib as well

--- build.rs
+++ build.rs
@@ -6,13 +6,18 @@ fn main() {
     let root_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    let lib = Path::new(&root_dir)
+    let lib_dir = Path::new(&root_dir)
         .join("interception-1.0.1")
-        .join("x64")
-        .join("interception.dll");
+        .join("x64");
 
-    let lib_out = Path::new(&out_dir).join("interception.dll");
-    fs::copy(&lib, &lib_out).unwrap();
+    let stlib = lib_dir.join("interception.lib");
+    let dylib = lib_dir.join("interception.dll");
+
+    let stlib_out = Path::new(&out_dir).join("interception.lib");
+    let dylib_out = Path::new(&out_dir).join("interception.dll");
+
+    fs::copy(&dylib, &dylib_out).unwrap();
+    fs::copy(&stlib, &stlib_out).unwrap();
 
     println!("cargo:rustc-link-search={}", out_dir);
     println!("cargo:rustc-link-lib=dylib=interception");
